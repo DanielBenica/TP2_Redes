875009df3aa8f4e0d9ea06474f63aede37518ee0
DanielBenica <danielbenica7@gmail.com>
2022-06-15 18:14:46 -0300

    First commit

---
+++ server-mt.c
@@ -0,0 +1,148 @@
+#include "common.h"
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define BUFSZ 1024
+#define MAX_CLIENTS 15  
+int Equipamentos[15];
+
+int addEquipament(char buf[BUFSZ]){
+   //Verificamos o numero total de equipamentos
+   int equipTotais = 0;
+   for(int i = 0; i < MAX_CLIENTS; i++){
+      if(Equipamentos[i] != 0){
+        equipTotais ++;
+      }
+   }
+    //Se o numero de equipamentos for 15 retornamos 0
+    if(equipTotais == 15){
+      sprintf(buf, "Equipment limit exceeded");
+      return 0;
+    }
+
+    //fazer o tratamento do ID aqui dentro
+   int NumEquip = 0;
+    //cria a novo id para o equipamento
+    for(int i = 0; i < MAX_CLIENTS; i++){
+        if(Equipamentos[i] == 0){
+            NumEquip = i+1;
+            Equipamentos[i] = i+1;
+            break;
+        }
+    }
+    // coloca o ID do equipamento no buffer
+    sprintf(buf, "New ID: 0%d", NumEquip);
+}
+
+
+void usage(int argc, char **argv) {
+    printf("usage: %s <v4|v6> <server port>\n", argv[0]);
+    printf("example: %s v4 51511\n", argv[0]);
+    exit(EXIT_FAILURE);
+}
+
+struct client_data {
+    int csock;
+    struct sockaddr_storage storage;
+};
+
+void * client_thread(void *data) {
+    struct client_data *cdata = (struct client_data *)data;
+    struct sockaddr *caddr = (struct sockaddr *)(&cdata->storage);
+
+    char caddrstr[BUFSZ];
+    addrtostr(caddr, caddrstr, BUFSZ);
+    printf("[log] connection from %s\n", caddrstr);
+
+    char buf[BUFSZ];
+    memset(buf, 0, BUFSZ);
+    //função para adicionar
+    addEquipament(buf);
+    puts(buf);
+    //colocar um send com a resposta
+
+    while(1){
+    memset(buf, 0, BUFSZ);
+    printf("[log] waiting for data\n");	
+    //Guarda em buf o que o cliente enviou
+    size_t count = recv(cdata->csock, buf, BUFSZ - 1, 0);
+    printf("[msg] %s, %d bytes: %s\n", caddrstr, (int)count, buf);
+   
+    puts(buf);
+    count = send(cdata->csock, buf, strlen(buf) + 1, 0);
+    if (count != strlen(buf) + 1) {
+        logexit("send");
+    }
+    }
+    close(cdata->csock);
+
+    pthread_exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        usage(argc, argv);
+    }
+
+    struct sockaddr_storage storage;
+    if (0 != server_sockaddr_init(argv[1], argv[2], &storage)) {
+        usage(argc, argv);
+    }
+
+    int s;
+    s = socket(storage.ss_family, SOCK_STREAM, 0);
+    if (s == -1) {
+        logexit("socket");
+    }
+
+    int enable = 1;
+    if (0 != setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int))) {
+        logexit("setsockopt");
+    }
+
+    struct sockaddr *addr = (struct sockaddr *)(&storage);
+    if (0 != bind(s, addr, sizeof(storage))) {
+        logexit("bind");
+    }
+
+    if (0 != listen(s, 10)) {
+        logexit("listen");
+    }
+
+    char addrstr[BUFSZ];
+    addrtostr(addr, addrstr, BUFSZ);
+    printf("bound to %s, waiting connections\n", addrstr);
+
+    //zeramos o vetor de equipamentos
+    memset(Equipamentos, 0, sizeof(Equipamentos));
+
+    while (1) {
+        struct sockaddr_storage cstorage;
+        struct sockaddr *caddr = (struct sockaddr *)(&cstorage);
+        socklen_t caddrlen = sizeof(cstorage);
+
+        int csock = accept(s, caddr, &caddrlen);
+        if (csock == -1) {
+            logexit("accept");
+        }
+
+	struct client_data *cdata = malloc(sizeof(*cdata));
+	if (!cdata) {
+		logexit("malloc");
+	}
+	cdata->csock = csock;
+	memcpy(&(cdata->storage), &cstorage, sizeof(cstorage));
+
+        pthread_t tid;
+        pthread_create(&tid, NULL, client_thread, cdata);
+    }
+
+    exit(EXIT_SUCCESS);
+}
